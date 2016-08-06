6f96f51092b3af71a26b2cb0defb424ab7560500
rawtoast <jim@jamesmorris.org>
2016-08-06 16:23:32 +0100

    Ch3. Tree exercises

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
@@ -7,20 +9,42 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  def size(t: Tree[_]): Int = sys.error("todo")
+  def size(tree: Tree[_]): Int =
+    tree match {
+      case (Leaf(value)) => 1
+      case (Branch(left, right)) => 1 + size(left) + size(right)
+    }
 
-  def maximum(t: Tree[Int]): Int = sys.error("todo")
+  def maximum(tree: Tree[Int]): Int =
+    tree match {
+      case (Leaf(value)) => value
+      case (Branch(left, right)) => maximum(left).max(maximum(right))
+    }
 
-  def depth(t: Tree[_]): Int = sys.error("todo")
+  def depth(tree: Tree[_]): Int =
+    tree match {
+      case (Leaf(value)) => 0
+      case (Branch(left, right)) => 1 + depth(left).max(depth(right))
+    }
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = sys.error("todo")
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+    tree match {
+      case (Leaf(value)) => Leaf(f(value))
+      case (Branch(left, right)) => Branch(map(left)(f), map(right)(f))
+    }
 
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = sys.error("todo")
+  def fold[A, B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B =
+    tree match {
+      case Leaf(v) => f(v)
+      case Branch(l: Tree[A], r: Tree[A]) => g(fold(l)(f)(g), fold(r)(f)(g))
+    }
 
-  def sizeViaFold[A](t: Tree[A]): Int = 
+  def sizeViaFold[A](t: Tree[A]): Int =
     fold(t)(a => 1)(1 + _ + _)
 
-  def maximumViaFold[T](t: Tree[T])(implicit ev: Numeric[T]): T = sys.error("todo")
+  def maximumViaFold[T](t: Tree[T])(implicit ev: Numeric[T]): T =
+    fold(t)(a => a)((x: T, y: T) => ev.max(x, y))
 
-  def depthViaFold[A](t: Tree[A]): Int = sys.error("todo")
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(a => 0)((x:Int, y:Int) => 1 + x.max(y))
 }
