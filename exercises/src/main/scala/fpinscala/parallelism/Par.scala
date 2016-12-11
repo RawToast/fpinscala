e471b3d50ed7d24887cb0e42290dcea9f7ccdfd4
rawtoast <jim@jamesmorris.org>
2016-12-11 23:48:40 +0000

    Ch7. Par exercises 1-4

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -9,7 +9,7 @@ object Par {
 
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
 
-  def lazyUnit[A](a: => A): Par[A] = ???
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true 
@@ -27,10 +27,13 @@ object Par {
   
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
-      def call = a(es).get
+      def call: A = a(es).get
     })
 
-  def asyncF[A,B](f: A => B): A => Par[B] = ???
+  // This API already enables a rich set of operations.
+  // Here’s a simple example: using lazyUnit, write a function to convert any function A => B
+  // to one that evaluates its result asynchronously.
+  def asyncF[A,B](f: A => B): A => Par[B] = (a:A) => lazyUnit(f(a))
 
   def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
     map2(pa, unit(()))((a,_) => f(a))
